a73423f533b1d25c442808c32a65020baa715d05
Labnan <khalidmasum@iut-dhaka.edu>
2021-06-26 19:21:51 +0600

    Login Page

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+const express = require('express');
+const path = require('path');
+let app = express();
+
+app.listen(8888);
+
+app.use(express.static('./AdminLTE'));
+
+app.get( '/login',(request,response)=>{
+   response.sendFile(path.resolve(__dirname,'./AdminLTE/pages/examples/login.html'));
+})
+
+
+console.log("Hello World!");

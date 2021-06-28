2b81b9ba36f3630a99ffe5b5f8c0e0fdc772a4d9
Labnan <khalidmasum@iut-dhaka.edu>
2021-06-28 13:29:09 +0600

    Handling login

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -1,12 +1,48 @@
 const path = require('path');
+const database = require('../database/database');
 
 function show(request,response){
     response.sendFile(path.resolve(__dirname,'./../public/AdminLTE/AdminLTE/pages/examples/login-v2.html'));
 }
 
+let requestData;
+
 function post(request,response){
-    console.log(request.body);
-    response.sendStatus(200);
+    requestData = request.body;
+    return  login(response);
+
+}
+
+function login(response){
+    console.log("DB Checking");
+    let sql = {}
+    sql.query = "select * from users where email = ? and password = ?";
+    sql.parameters = [requestData.email, requestData.password];
+    return  processLogin(makeAsyncQuery(sql),response);
+
+
+}
+
+function processLogin(queryPromise,response){
+    queryPromise.then((rows)=>{
+        console.log(requestData);
+        if(rows.length==1){
+           return response.sendFile(path.resolve(__dirname,'./../public/AdminLTE/AdminLTE/index.html'));
+        }
+    });
+
+}
+
+
+const makeAsyncQuery = async (sql)=>{
+    try {
+        const first = await database.query(sql);
+        return first;
+    }
+    catch (err){
+        throw err;
+    }
+
 }
 
 module.exports = {show,post}
--- src/database/database.js
+++ src/database/database.js
@@ -1,2 +1,24 @@
-const sqlite3 = require('sqlite3').verbose();;
-let db = new sqlite3.Database(":memory");
+const sqlite3 = require('sqlite3').verbose();
+const util = require('util');
+let db = new sqlite3.Database("./database/SimpleAuth.db",(err)=>{console.log(err)});
+
+function close(){
+    db.close();
+}
+
+
+  function query(sql){
+    return new Promise(((resolve, reject) => {
+        db.all(sql.query, sql.parameters,(err, rows) => {
+            if (err) {
+                reject(err);
+            }
+            else resolve(rows);
+        });
+    }))
+
+}
+
+promiseQuery = util.promisify(query);
+
+module.exports={query,close,promiseQuery};

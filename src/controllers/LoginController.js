5bf657c2d3b96b05cbc0e7b3001af4a29b80bab2
Labnan <khalidmasum@iut-dhaka.edu>
2021-06-30 10:35:56 +0600

    Display Name Provided

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const database = require('../database/database');
 const bcrypt = require('bcrypt');
+const fs = require('fs');
 
 function show(request,response){
     response.sendFile(path.resolve(__dirname,'./../public/AdminLTE/AdminLTE/pages/examples/login-v2.html'));
@@ -23,13 +24,22 @@ function login(response){
 
 }
 
+function makeLogin(response, rows) {
+    fs.readFile(path.resolve(__dirname, './../views/dashboard.html'),((err, data) => {
+        let toSend = data.toString().replace("/*$$RESPONSE_DATA$$*/",`let username = "${rows[0].name}"`);
+        return response.send(toSend);
+    }))
+
+}
+
 function processLogin(queryPromise,response){
 
     queryPromise.then((rows)=>{
         console.log(loginCredentials);
-        if(rows[0]!=null)
-        {if(bcrypt.compareSync(loginCredentials.password,rows[0].password))
-           return response.sendFile(path.resolve(__dirname,'./../views/dashboard.html'));}
+        if(rows[0]!=null) {
+            if(bcrypt.compareSync(loginCredentials.password,rows[0].password))
+            return makeLogin(response, rows);
+        }
         else return response.redirect('/login');
     });
 }
